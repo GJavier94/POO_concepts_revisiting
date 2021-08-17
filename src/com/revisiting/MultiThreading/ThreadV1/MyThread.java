759b0368f895315df54ab0a236f7e5c64cc000de
Javier Armenta <javarmgar@gmail.com>
2021-08-17 12:48:27 -0500

    This first version shows how a thread is created inherit from the class Thread or implements runnable interface so that it  needs to implements the method public void run() : that it where thread's code will be running if being implemented using interface Runnable it necesarry to create an instance of the class and then use Thread constructor which receives a runnable object then execute method start

---
+++ src/com/revisiting/MultiThreading/ThreadV1/MyThread.java
@@ -0,0 +1,23 @@
+package com.revisiting.MultiThreading.ThreadV1;
+
+public class MyThread implements Runnable{
+    private int count;
+    private String thrName;
+    MyThread(String name){
+        this.thrName = name;
+        this.count = 0;
+    }
+    @Override
+    public void run() {
+        for(int i = 0; i < 10 ; i++){
+            try{
+                Thread.sleep(400);
+                System.out.println(this.thrName + " "+ (++count));
+            }catch (InterruptedException e ){
+                System.out.println(this.thrName + " "  + "interruption occurred");
+            }
+
+        }
+        System.out.println(this.thrName + " terminating...");
+    }
+}
