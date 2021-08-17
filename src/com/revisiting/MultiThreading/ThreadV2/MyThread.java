c53a88be723d2c4317ed0ccceaced4f0ccacebd6
Javier Armenta <javarmgar@gmail.com>
2021-08-17 13:04:05 -0500

    implementing a factory method to create a Thread object and also run it

---
+++ src/com/revisiting/MultiThreading/ThreadV2/MyThread.java
@@ -0,0 +1,37 @@
+package com.revisiting.MultiThreading.ThreadV2;
+
+public class MyThread implements Runnable{
+    private int count;
+    private String thrName;
+    // creating a reference of this class
+    private Thread thr;
+
+    MyThread(String name){
+        this.thrName = name;
+        this.thr = new Thread(this,name);
+        this.count = 0;
+    }
+    /*
+    Using a factory method to create and start a Thread object
+    so that it's easier when creating multiple objects of this class
+
+    * */
+    public static MyThread createAndStart(String name){
+        MyThread myThrd = new MyThread(name);
+        myThrd.thr.start();
+        return myThrd;
+    }
+    @Override
+    public void run() {
+        for(int i = 0; i < 10 ; i++){
+            try{
+                Thread.sleep(400);
+                System.out.println(this.thrName + " "+ (++count));
+            }catch (InterruptedException e ){
+                System.out.println(this.thrName + " "  + "interruption occurred");
+            }
+
+        }
+        System.out.println(this.thrName + " terminating...");
+    }
+}
---
+++ src/com/revisiting/MultiThreading/ThreadV2/UseThreads.java
@@ -0,0 +1,21 @@
+package com.revisiting.MultiThreading.ThreadV2;
+
+public class UseThreads {
+    public static void main(String[] args) {
+        //Creating and running one thread from the main thread
+        // in this case main thread makes execution ona  for which goes to sleep state
+        // so that the main thread finished later than the Thread #1
+        MyThread myThread = MyThread.createAndStart("Thread#1");
+
+        for(int i = 0 ; i < 50 ; i++){
+            System.out.println("...");
+            try{
+                Thread.sleep(100);
+            }catch(InterruptedException e ){
+                System.out.println("Main Thread Interrupted..");
+            }
+        }
+        System.out.println("Main thread ending....");
+    }
+
+}
