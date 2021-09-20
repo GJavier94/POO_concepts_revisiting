bd6950d819f1e80c77cc2f666d2d9e4f67fe1f7d
Javier Armenta <javarmgar@gmail.com>
2021-09-20 17:22:24 -0500

    /**                  * COMPOSING MULTIPLE FLOWS                  * zip , combine                  */

--- src/main/kotlin/CoroutinesFlowBuffering.kt
+++ src/main/kotlin/CoroutinesFlowBuffering.kt
@@ -90,11 +90,101 @@ class CoroutinesFlowBuffering {
                     }
                     log("timeWhenLatest:$timeWhenLatest")
                 }
+                job4.join()
+
+
+                //any iterable which is a list of pairs can be turn to map by toMap() function
+                /*val list = listOf(
+                    Pair(1, "uno"),
+                        Pair(2,"dos"),
+                        Pair(3,"tres"))
+                list.toMap()*/
+
+                // we can create two sequences
+                val sequence1 = sequenceOf(1,2,3)
+                val sequence2 = sequenceOf("uno","dos","tres")
+                sequence1.zip(sequence2).toMap().forEach{ log("key:${it.key}, value:${it.value}")}
+                /**
+                 * if we made this with sequence we can also do it with
+                 * flows
+                 */
+
+                /**
+                 * COMPOSING MULTIPLE FLOWS
+                 * zip , combine
+                 */
+                val job5 = launch {
+                    val flow1:Flow<Int> = produceSomething(170L,3 )
+                    val flow2:Flow<Int> = produceSomething(160L,3 )
+                    //Zip needs the other flow and the transform functiosn wich will produces the values by using the flows
+                    //and then will convert them
+                    flow1.zip(flow2){
+                        v1,v2 ->
+                        "v1:$v1, v2:$v2" // it is producing a list of strings
+                    }.toList().forEach { log(it) }
+                    /**
+                     * Let's define F as the set of flows
+                     * zip is a function such that
+                     *      zip: F -> F
+                     *          f1.zip(f2) -> f3
+                     *  then you can apply a final operation
+                     */
+                    // zip makes synchronized it waits for the other value to ve collected
+                    // but what if we want to use conflated -> we use combined so every time there's an update we get a new pair
+                    flow1.combine(flow2){
+                            v1,v2 ->
+                        "v1:$v1, v2:$v2"
+                    }.collect { log(it) }
+                }
+                job5.join()
+                /**
+                 * [main @coroutine#16] v1:0, v2:0
+                    [main @coroutine#16] v1:0, v2:1
+                    [main @coroutine#16] v1:1, v2:1
+                    [main @coroutine#16] v1:1, v2:2
+                    [main @coroutine#16] v1:2, v2:2
+                 */
+
+                /**
+                 * We flows basically have the pattern request response
+                 * we can nest flows ( make flow of flows )  request response request
+                 */
+
+                val flowNames:Flow<String> = generateNames()
+
+                flowNames.flatMapConcat {
+                    generateNumbers(it)
+                }.collect {
+                    value ->
+                    log("concatenated flatmap value $value")
+                }
+                // we had a flow of flows
+
             }
         }
 
-        private fun produceSomething(timeEmiter: Long): Flow<Int> = flow {
+        private fun generateNames(): Flow<String> = flow{
             repeat(5){
+                i ->
+                delay(1000L)
+                emit ( when(i){
+                    0 -> "Javier"
+                    1 -> "Yvett"
+                    2 -> "Nicolas"
+                    3 -> "Daniel"
+                    else -> "Ernesto"
+                })
+            }
+        }
+        private fun generateNumbers(name: String):Flow<String> = flow{
+            repeat(5){
+                delay(900L)
+                emit("$it: Name $name" )
+            }
+        }
+
+        private fun produceSomething(timeEmiter: Long, size:Int = 5): Flow<Int> = flow {
+            repeat(size){
                 log("$it emit(): producing..")
                 delay(timeEmiter)
                 log("$it emit(): production done... Sending production")
@@ -103,5 +193,4 @@ class CoroutinesFlowBuffering {
         }.flowOn(Dispatchers.Default)
 
     }
-
 }
